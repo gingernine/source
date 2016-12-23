e5cd4e4ff0e023e75021e11461090d017ee2a607
gingernine <sy44895@gmail.com>
2016-12-23 10:14:09 +0900

    2016/12/23

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 2016/12/23
+ *      Author: kklab
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello C++ World";
+    return 0;
+}
